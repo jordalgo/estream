7e8d61f93849dbb1202dcd6d17319f827b3a8345
Jordalgo <jordalgo@spotify.com>
2016-04-14 22:53:24 -0400

    Add: additional unit tests

--- test/index.js
+++ test/index.js
@@ -34,4 +34,87 @@ describe('sources', function() {
       done();
     }, 10);
   });
+
+  it('throws if you call next if the stream has completed', function() {
+    var s = PH.source(function(next, end, complete) {
+      complete();
+      try {
+        next(1);
+        assert.fail();
+      } catch (e) {
+        assert.equal(typeof e, 'object');
+      }
+    });
+    s.start();
+  });
+
+  it('throws if you call error if the stream has completed', function() {
+    var s = PH.source(function(next, end, complete) {
+      complete();
+      try {
+        error(1);
+        assert.fail();
+      } catch (e) {
+        assert.equal(typeof e, 'object');
+      }
+    });
+    s.start();
+  });
+
+  it('passes values to multiple pipes', function(done) {
+    var pipeCalled = 0;
+    var s = PH.source(function(next, end, complete) {
+      next(5);
+      complete();
+    });
+
+    PH.pipe(s)
+    .onNext(function(x) {
+      assert.equal(x, 5);
+      pipeCalled++;
+    });
+    PH.pipe(s)
+    .onNext(function(x) {
+      assert.equal(x, 5);
+      pipeCalled++;
+    });
+    PH.pipe(s)
+    .onNext(function(x) {
+      assert.equal(x, 5);
+      pipeCalled++;
+    })
+    .onComplete(function() {
+      assert.equal(pipeCalled, 3);
+      done();
+    });
+
+    s.start();
+  });
+
+  it('does not pass values when not open', function(done) {
+    var s = PH.source(function(next, end, complete) {
+      setTimeout(function() {
+        next(5);
+        s.open();
+      }, 50);
+      setTimeout(function() {
+        next(6);
+      }, 100);
+    });
+
+    PH.pipe(s)
+    .onNext(function(x) {
+      assert.equal(x, 6);
+      done();
+    });
+
+    s.start();
+    s.close();
+  });
 });
+
+// describe('pipes', function() {
+//   it('')
+
+// });
+
