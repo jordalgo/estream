6b3aad405c6fd6d8e1af224e023eec54f901e1bf
Jordalgo <jordalgo@spotify.com>
2016-07-18 21:43:08 -0400

    Add: stop method

--- test/index.js
+++ test/index.js
@@ -11,12 +11,49 @@ describe('Estream', function() {
     assert.equal(typeof estream, 'function');
   });
 
+  it('has a method to push data into it and a consume that data', function(done) {
+    var s = estream({
+      start: function(push) {
+        push(5);
+      }
+    });
+    s.on(function(data) {
+      assert.equal(data.isData, true);
+      assert.equal(data.value, 5);
+      done();
+    });
+  });
+
+  it('routes errors', function(done) {
+    var s = estream({
+      start: function(push, error) {
+        error('error');
+      }
+    });
+    s.on(function(err) {
+      assert.equal(err.isError, true);
+      assert.equal(err.value, 'error');
+      done();
+    });
+  });
+
+  it('routes ends', function(done) {
+    var s = estream({
+      start: function(push, error, end) {
+        end('byebye');
+      }
+    });
+    s.on(function(end) {
+      assert.equal(end.isEnd, true);
+      assert.deepEqual(end.value, ['byebye']);
+      done();
+    });
+  });
+
+
   it('calls the passed function when a subscriber is added', function(done) {
     var called = false;
-    var s = estream(function(self) {
-      called = true;
-      self.push(5);
-    });
+    var s = estream({ start: function(push) { called = true; push(5); } });
     assert.ok(!called);
     s.on(function(data) {
       assert.equal(data.isData, true);
@@ -27,109 +64,199 @@ describe('Estream', function() {
   });
 
   it('wont push an event that has occured before a subscriber is added', function(done) {
-    var s = estream();
-    s.push(1);
+    var s = estream({
+      start: function(push) {
+        push(5);
+      }
+    });
     s.on(function(data) {
       assert.equal(data.isData, true);
       assert.equal(data.value, 5);
       done();
     });
-    s.push(5);
   });
 
   it('always sends the history when a new event occurs', function(done) {
-    var s = estream(function(self) {
-      self.push(4);
-    }, { history: 2 });
-    s.push(1);
-    s.push(2);
+    var s = estream(
+      {
+        start: function(push) {
+          push(1);
+          push(2);
 
-    s.on(function(event, history) {
-      assert.equal(history[0].value, 1);
-      assert.equal(history[1].value, 2);
-      assert.equal(event.value, 4);
-      done();
+          setTimeout(push.bind(null, 4), 500);
+        }
+      },
+      { history: 2 }
+    );
+
+    // start the stream flowing
+    s.on(function() {});
+
+    setTimeout(function() {
+      s.on(function(event, history) {
+        assert.equal(history[0].value, 1);
+        assert.equal(history[1].value, 2);
+        assert.equal(event.value, 4);
+        done();
+      });
+    }, 100);
+  });
+
+  it('will call stop if there are no more subscribers', function(done) {
+    var s = estream({
+      start: function(push) { push(1); },
+      stop: function() {
+        done();
+      }
+    });
+
+    s.on(function(d, h, _, off) {
+      off();
     });
   });
 
-  it('has a method to push data into it and a consume that data', function(done) {
-    var s = estream(function(self) {
-      self.push(5);
+  it('passes the return of start to the stop function', function(done) {
+    var s = estream({
+      start: function(push) { push(1); return 'hello'; },
+      stop: function(artifact) {
+        assert.equal(artifact, 'hello');
+        done();
+      }
     });
-    s.on(function(data) {
-      assert.equal(data.isData, true);
-      assert.equal(data.value, 5);
-      done();
+
+    s.on(function(d, h, _, off) {
+      off();
     });
   });
 
-  it('routes errors', function(done) {
-    var s = estream(function(self) {
-      self.error('error');
+  it('will call re call start if new subscribers are added after stop', function(done) {
+    var startCalled = 0;
+    var s = estream({
+      start: function(push) {
+        startCalled++;
+        if (startCalled === 2) {
+          done();
+        } else {
+          push(1);
+        }
+      }
     });
-    s.on(function(err) {
-      assert.equal(err.isError, true);
-      assert.equal(err.value, 'error');
-      done();
+
+    s.on(function(d, h, _, off) {
+      off();
+      setTimeout(function() {
+        s.on(function() {});
+      }, 500);
     });
   });
 
-  it('routes ends', function(done) {
-    var s = estream(function(self) {
-      self.end('byebye');
+  it('wont call stop if a subscriber is added ' +
+  'in the same stack as a subscriber is removed', function(done) {
+    var s = estream({
+      start: function(push) {
+        push(3);
+        setTimeout(function() {
+          push(4);
+        }, 500);
+      },
+      stop: function() {
+        assert.fail();
+      }
     });
-    s.on(function(end) {
-      assert.equal(end.isEnd, true);
-      assert.deepEqual(end.value, ['byebye']);
-      done();
+
+    var off = s.on(function(x) {
+      assert.equal(x.value, 3);
     });
+
+    setTimeout(function() {
+      off();
+      s.on(function(y) {
+        assert.equal(y.value, 4);
+        done();
+      });
+    }, 100);
   });
 
   it('can combine multiple source estreams', function(done) {
-    var s1 = estream();
-    setTimeout(s1.push.bind(s1, 1));
-    setTimeout(s1.end.bind(s1, 'hello'), 150);
-    var s2 = estream();
-    setTimeout(s2.push.bind(s2, 10));
-    setTimeout(s2.error.bind(s2, 'error'), 50);
-    setTimeout(s2.end.bind(s2, 'bye'), 300);
+    var s1 = estream({
+      start: function(push) {
+        push(1);
+        setTimeout(function() {
+          push('hello');
+        }, 100);
+      }
+    });
+    var s2 = estream({
+      start: function(push) {
+        push(2);
+        setTimeout(function() {
+          push('bye');
+        }, 200);
+      }
+    });
     var s3 = estream.combine([s1, s2]);
-    var s4 = estream.combine([s3]);
-    var called = 0;
 
+    var called = 0;
     s3
     .on(function(x, _, sourceEstream) {
       if (called === 0) {
         assert.equal(x.value, 1);
         assert.equal(sourceEstream, s1);
       } else if (called === 1) {
-        assert.equal(x.value, 10);
+        assert.equal(x.value, 2);
         assert.equal(sourceEstream, s2);
       } else if (called === 2) {
-        assert.equal(x.value, 'error');
-        assert.equal(sourceEstream, s2);
+        assert.equal(x.value, 'hello');
+        assert.equal(sourceEstream, s1);
       } else if (called === 3) {
-        assert.deepEqual(x.value, ['hello', 'bye']);
-        assert.equal(sourceEstream, s3);
+        assert.deepEqual(x.value, 'bye');
+        assert.equal(sourceEstream, s2);
         done();
       }
       called++;
     });
+  });
 
-    s4.on(function(x, _, sourceEstream, off) {
-      if (called === 0) {
-        assert.equal(x.value, 1);
-        assert.equal(sourceEstream, s1);
+  it('calls stop on both source streams when the combined subscriber is removed', function(done) {
+    var s1StopCalled = false;
+    var s2StopCalled = false;
+    var s1 = estream({
+      start: function(push) {
+        push(1);
+      },
+      stop: function() {
+        s1StopCalled = true;
       }
+    });
+    var s2 = estream({
+      start: function(push) {
+        push(2);
+      },
+      stop: function() {
+        s2StopCalled = true;
+      }
+    });
+
+    estream
+    .combine([s1, s2])
+    .on(function(x, _, sourceEstream, off) {
       off();
     });
+
+    setTimeout(function() {
+      assert(s1StopCalled);
+      assert(s2StopCalled);
+      done();
+    }, 100);
   });
 
   it('has helper methods for consuming error and end messages', function(done) {
-    var s = estream(function(self) {
-      self.push(5);
-      self.error('error');
-      self.end('bye');
+    var s = estream({
+      start: function(push, error, end) {
+        push(5);
+        error('error');
+        end('bye');
+      }
     });
     var errorCalled;
 
@@ -146,18 +273,19 @@ describe('Estream', function() {
 
   it('passes unsubscribe functions to consumers when events are emitted', function(done) {
     var called;
-    var s = estream();
-    setTimeout(function() {
-      s.push(1);
-      assert.ok(called);
-      s.error(new Error('boom'));
+    var s = estream({
+      start: function(push, error) {
+        push(1);
+        assert.ok(called);
+        error(new Error('boom'));
+      }
     });
 
-    s.on(function(x, _, sref, unsubscribe) {
+    s.on(function(x, _, sref, off) {
       if (!called) {
         assert.equal(x.value, 1);
         assert.equal(sref, s);
-        unsubscribe();
+        off();
         called = true;
       } else {
         assert.fail();
@@ -168,8 +296,10 @@ describe('Estream', function() {
   });
 
   it('emits unique event objects', function(done) {
-    var s = estream(function(self) {
-      self.push(5);
+    var s = estream({
+      start: function(push) {
+        push(5);
+      }
     });
 
     s.on(function(event) {
@@ -186,19 +316,27 @@ describe('Estream', function() {
   });
 
   it('returns the history with `getHistory`', function(done) {
-    var s = estream(null, { history: 100 });
-    s.push(3);
-    s.push(5);
-    s.push(4);
-    setTimeout(s.error.bind(s, 'boom'), 100);
+    var s = estream({
+      start: function(push, error) {
+        push(3);
+        push(5);
+        push(4);
+        setTimeout(error.bind(s, 'boom'), 200);
+      }
+    }, { history: 100 });
 
-    var history = s.getHistory();
-    assert.equal(history[0].value, 3);
-    assert.equal(history[0].isData, true);
-    assert.equal(history[1].value, 5);
-    assert.equal(history[2].value, 4);
+    s.on(function() {});
 
-    assert.equal(s._history.length, 3);
+    var history;
+
+    setTimeout(function() {
+      history = s.getHistory();
+      assert.equal(history[0].value, 3);
+      assert.equal(history[0].isData, true);
+      assert.equal(history[1].value, 5);
+      assert.equal(history[2].value, 4);
+      assert.equal(s._history.length, 3);
+    }, 100);
 
     setTimeout(function() {
       history = s.getHistory();
@@ -208,19 +346,25 @@ describe('Estream', function() {
       assert.equal(history[3].value, 'boom');
       assert.equal(history[3].isError, true);
       done();
-    }, 200);
+    }, 500);
   });
 
   it('clears the history with `clearhistory`', function() {
-    var s = estream(null, { history: 3 });
-    s.push(3);
-    s.push(5);
-    s.push(4);
+    var s = estream({
+      start: function(push) {
+        push(3);
+        push(5);
+        push(4);
+      }
+    }, { history: 100 });
 
-    assert.equal(s.getHistory().length, 3);
+    s.on(function() {});
 
-    s.clearHistory();
-    assert.deepEqual(s.getHistory(), []);
+    setTimeout(function() {
+      assert.equal(s.getHistory().length, 3);
+      s.clearHistory();
+      assert.deepEqual(s.getHistory(), []);
+    }, 100);
   });
 
   it('has an addMethods method', function() {
@@ -228,27 +372,30 @@ describe('Estream', function() {
       name: 'fmap',
       fn: function() {}
     }]);
-    var p = estream();
+    var p = estream({ start: function() {} });
     assert.equal(typeof p.fmap, 'function');
   });
 
   it('doesnt keep a history if no history number is passed as an option', function(done) {
-    var s = estream(null);
-    s.push(3);
-
-    s.on(function() {
-      assert.fail();
+    var s = estream({
+      start: function(push) {
+        push(3);
+      }
     });
+
+    s.on(function() { });
 
     setTimeout(function() {
       assert.equal(s.getHistory().length, 0);
       done();
-    }, 10);
+    }, 100);
   });
 
   it('is a functor', function(done) {
-    var s = estream(function(self) {
-      self.push(1);
+    var s = estream({
+      start: function(push) {
+        push(1);
+      }
     });
     var add2 = function(x) { return x + 2; };
     var composedMap = R.pipe(add1, add2);
@@ -281,9 +428,11 @@ describe('Estream', function() {
   describe('map', function() {
     it('maps a function over data values', function(done) {
       var called = 0;
-      var s1 = estream(function(self) {
-        self.error('error');
-        self.push(4);
+      var s1 = estream({
+        start: function(push, error) {
+          error('error');
+          push(4);
+        }
       });
 
       s1
@@ -299,11 +448,30 @@ describe('Estream', function() {
       });
     });
 
+    it('stops the parent stream if you remove the map subscriber', function(done) {
+      var s1 = estream({
+        start: function(push) {
+          push(4);
+        },
+        stop: function() {
+          done();
+        }
+      });
+
+      s1
+      .map(add1)
+      .on(function(x, h, st, off) {
+        off();
+      });
+    });
+
     it('can have a history', function(done) {
       var called = 0;
-      var s1 = estream(function(self) {
-        self.error('error');
-        self.push(4);
+      var s1 = estream({
+        start: function(push, error) {
+          error('error');
+          push(4);
+        }
       });
 
       s1
@@ -324,9 +492,11 @@ describe('Estream', function() {
   describe('scan', function() {
     it('reduces pushed data events', function(done) {
       var called = 0;
-      var s = estream(function(self) {
-        self.push(5);
-        self.push(10);
+      var s = estream({
+        start: function(push) {
+          push(5);
+          push(10);
+        }
       });
 
       s.scan(sum, 5)
@@ -340,14 +510,33 @@ describe('Estream', function() {
         }
       });
     });
+
+    it('stops the parent stream if you remove the subscriber', function(done) {
+      var s1 = estream({
+        start: function(push) {
+          push(4);
+        },
+        stop: function() {
+          done();
+        }
+      });
+
+      s1
+      .scan(sum, 5)
+      .on(function(x, h, st, off) {
+        off();
+      });
+    });
   });
 
   describe('filter', function() {
     it('filters data event values', function(done) {
       var called = 0;
-      var s = estream(function(self) {
-        self.push(5);
-        self.push(11);
+      var s = estream({
+        start: function(push) {
+          push(5);
+          push(11);
+        }
       });
 
       s.filter(isGt10)
@@ -355,6 +544,23 @@ describe('Estream', function() {
         assert.equal(x.value, 11);
         assert.equal(called, 0);
         done();
+      });
+    });
+
+    it('stops the parent stream if you remove the subscriber', function(done) {
+      var s1 = estream({
+        start: function(push) {
+          push(14);
+        },
+        stop: function() {
+          done();
+        }
+      });
+
+      s1
+      .filter(isGt10)
+      .on(function(x, h, st, off) {
+        off();
       });
     });
   });
